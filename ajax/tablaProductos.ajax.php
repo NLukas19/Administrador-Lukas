5683983ac1513f55b94ba40fd610c2eab6ee7d4a
NLukas19 <lukasnahuel241@gmail.com>
2024-06-18 18:34:29 -0300

    Agregado del boton de imprimir datos del producto

--- ajax/tablaProductos.ajax.php
+++ ajax/tablaProductos.ajax.php
@@ -35,7 +35,10 @@ class TablaProductos
             $categoria = CategoriasControlador::ctrMostrarCategorias("id_categoria", $productos[$i]["categoria_producto"]);
             $imagen = "<img width='100' src='".$productos[$i]['imagen_producto']."'>";
             //Traemos las acciones
-            $acciones = "<a type='button' class='btn btn-icon btn-light-warning btnBoton'  href = 'index.php?pagina=editar-producto&id_producto=" . $productos[$i]["id_producto"] . "' ><i class='fas fa-pen'></i></a> <button type='button' id_producto = '" . $productos[$i]["id_producto"] . "' class='btn btn-icon btn-light-danger btnEliminarProducto'><i class='fas fa-trash-alt'></i></button>";
+            $acciones = 
+            "<button type='button' id_producto = ". $productos[$i]["id_producto"] . "' class='btn btn-icon btn-light-info btnImprimirProducto'><i class='fas fa-file-archive'></i></button>".
+            "<a type='button' class='btn btn-icon btn-light-warning btnBoton'  href = 'index.php?pagina=editar-producto&id_producto=" 
+            . $productos[$i]["id_producto"] . "' ><i class='fas fa-pen'></i></a> <button type='button' id_producto = '". $productos[$i]["id_producto"] . "' class='btn btn-icon btn-light-danger btnEliminarProducto'><i class='fas fa-trash-alt'></i></button>";
            /*  $acciones = "<button type='button' class='btn bg-gradient-warning btnBoton' tipo = 'editar' id_producto = '" . $productos[$i]["id_producto"] . "'  data-toggle='modal' data-target='#editar-producto'><i class='fas fa-edit'></i></button> <button type='button' id_producto = '" . $productos[$i]["id_producto"] . "' class='btn bg-gradient-danger btnEliminarProducto'><i class='fas fa-trash'></i></button>"; */
 
             $datosJson .= '[
--- vistas/modulos/home.php
+++ vistas/modulos/home.php
@@ -1,3 +1,12 @@
+<?php
+
+$categorias = CategoriasControlador::ctrMostrarCategorias(null, null);
+#print_r($categorias);
+
+$productos = ProductosControlador::ctrMostrarProductos(null,null);
+
+?>
+
 <div class="pc-container">
     <div class="pc-content">
         <div class="row">
@@ -27,73 +36,36 @@
             <div class="col-md-12">
                 <div class="card table-card latest-activity-card">
                     <div class="card-header">
-                        <h5>Historial de ordenes</h5>
+                        <h5>Ultimos productos agregados</h5>
                     </div>
                     <div class="card-body">
                         <div class="table-responsive">
                             <table class="table table-hover table-borderless">
                                 <thead>
                                     <tr>
-                                        <th>Tienda</th>
-                                        <th>ID de la orden</th>
+                                        <th>Nombre</th>
+                                        <th>ID del producto</th>
                                         <th>Producto</th>
-                                        <th>Nombre del producto</th>
+                                        <th>Categoria</th>
                                         <th>Cantidad</th>
                                         <th>Fecha</th>
-                                        <th>Estatus</th>
                                     </tr>
                                 </thead>
+                                <?php foreach ($productos as $value) { ?>
                                 <tbody>
                                     <tr>
-                                        <td>MusiMundo</td>
-                                        <td>#81412314</td>
+                                        <td><?php echo $value["nombre_producto"];?></td>
+                                        <td><?php echo $value["id_producto"];?></td>
                                         <td><img src="<?php echo $url ?>vistas/assets/images/widget/PHONE1.jpg" alt=""
                                                 class="img-fluid">
                                         </td>
-                                        <td>Moto G5</td>
-                                        <td>10</td>
-                                        <td>17-2-2017</td>
-                                        <td><label class="badge bg-light-warning">Pendiente de pago</label></td>
+                                        <td>nombre categoria</td>
+                                        <td>stock que no puse</td>
+                                        <td><?php echo $value["fecha_creacion_producto"];?></td>
                                     </tr>
-                                    <tr>
-                                        <td>ElectroMecanicaJose</td>
-                                        <td>#68457898</td>
-                                        <td><img src="<?php echo $url ?>vistas/assets/images/widget/PHONE2.jpg" alt=""
-                                                class="img-fluid">
-                                        </td>
-                                        <td>iPhone 8</td>
-                                        <td>16</td>
-                                        <td>20-2-2017</td>
-                                        <td><label class="badge bg-light-primary">Enviado</label></td>
-                                    </tr>
-                                    <tr>
-                                        <td>Centrogar</td>
-                                        <td>#45457898</td>
-                                        <td><img src="<?php echo $url ?>vistas/assets/images/widget/PHONE3.jpg" alt=""
-                                                class="img-fluid">
-                                        </td>
-                                        <td>Redmi 4</td>
-                                        <td>20</td>
-                                        <td>17-2-2017</td>
-                                        <td><label class="badge bg-light-success">Pagado</label></td>
-                                    </tr>
-                                    <tr>
-                                        <td>RespuestoLosHermanos</td>
-                                        <td>#62446232</td>
-                                        <td><img src="<?php echo $url ?>vistas/assets/images/widget/PHONE4.jpg" alt=""
-                                                class="img-fluid">
-                                        </td>
-                                        <td>Jio</td>
-                                        <td>15</td>
-                                        <td>25-4-2017</td>
-                                        <td><label class="badge bg-light-danger">Cancelado</label></td>
-                                    </tr>
-
                                 </tbody>
+                                <?php } ?>
                             </table>
-                        </div>
-                        <div class="text-end  m-r-20">
-                            <a href="ordenes" class="b-b-primary text-primary">Ver todas las ordenes</a>
                         </div>
                     </div>
                 </div>
